df1b94f8cac8b79516ae513ddbc5c46e7e925174
Nicholas Chum <nicholaschum@gmail.com>
2015-03-29 03:41:56 -0400

    Initial commit with contributors

---
+++ java/com/chummy/jezebel/material/dark/MainActivity.java
@@ -0,0 +1,19 @@
+package com.chummy.jezebel.material.dark;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, com.jahirfiquitiva.paperboard.activities.Main.class);
+        startActivity(intent);
+    }
+}
